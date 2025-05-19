a95be93adb32153b0bd36ff27c0fa0fe76c50146
Andriy <grushevoyandrey@gmail.com>
2025-05-19 17:23:07 +0100

    Fix code style issues according to PEP8

--- car/serializers.py
+++ car/serializers.py
@@ -12,16 +12,34 @@ class CarSerializer(serializers.Serializer):
         validators=[MinValueValidator(1), MaxValueValidator(1914)]
     )
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(allow_null=True, required=False)
+    problem_description = serializers.CharField(
+        allow_null=True,
+        required=False
+    )
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.manufacturer = validated_data.get("manufacturer", instance.manufacturer)
-        instance.model = validated_data.get("model", instance.model)
-        instance.horse_powers = validated_data.get("horse_power", instance.horse_powers)
-        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
-        instance.problem_description = validated_data.get("problem_description", instance.problem_description)
+        instance.manufacturer = validated_data.get(
+            "manufacturer",
+            instance.manufacturer
+        )
+        instance.model = validated_data.get(
+            "model",
+            instance.model
+        )
+        instance.horse_powers = validated_data.get(
+            "horse_power",
+            instance.horse_powers
+        )
+        instance.is_broken = validated_data.get(
+            "is_broken",
+            instance.is_broken
+        )
+        instance.problem_description = validated_data.get(
+            "problem_description",
+            instance.problem_description
+        )
         instance.save()
         return instance
